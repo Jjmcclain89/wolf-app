4ca3b5c2d1009a2a9f9fac597dd20bd0e41e22b5
Jjmcclain89 <joshmcclain1989@gmail.com>
2020-09-30 20:08:50 -0600

    set up initial websockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Head from './components/Head';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const client = new W3CWebSocket('ws://localhost:1337');
+
+    useEffect(() => {
+        client.onopen = () => {
+            console.log('WebSocket Client Connected');
+        };
+        
+        client.onmessage = (message) => {
+            console.log(message);
+        };
+    }, [client.onopen, client.onmessage]);
+
+    return (
+        <>
+            <Head />
+            <div className='App'>
+                <Form className='col col-xs-12 col-4'>
+                    <Form.Group>
+                        <Form.Label> Name </Form.Label>
+                        <Form.Control type='text' placeholder='Name' />
+                    </Form.Group>
+                    <Button> Join Game </Button>
+                </Form>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ client/src/components/Head.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Head = (props) => {
+    return (
+        <head>
+            <link
+                rel='stylesheet'
+                href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+                integrity='sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z'
+                crossOrigin='anonymous'
+            />
+        </head>
+    );
+};
+
+export default Head;
